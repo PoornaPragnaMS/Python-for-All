039f345eccdd11c291beaad04853833a905ac3bc
PoornaPragnaMS <poorna220192@gmail.com>
2017-08-08 16:26:13 +0530

    Create PackingUnpackingInPython.py

---
+++ PackingUnpackingInPython.py
@@ -0,0 +1,34 @@
+##In this program we are going to see iterables in python and how we can pack and unpack them
+
+#--------------------------Let us consider a primitve iterable data type as our first case: String ---------------------------
+
+#Request an input string from End User
+
+#strip function is used to remove trailing and leading spaces around the string
+
+inputStr = input("Enter your input to unpack it :").strip() #Assume the input entered is "Hello"
+
+print(inputStr,sep=",") # printing without unpacking the string, Response here is "Hello"
+print(*inputStr,sep=",") #printing after unpacking the string, Response here is "H,e,l,l,o" due to iterable property of string
+
+#---------------------------Let us consider the second iterable type List for unpacking operation------------------------------
+inputList = ["Mango","Orange","Apple"]
+print(*inputList)
+
+#----------------------------Unpacking the range function----------------------------------------------------------------------
+
+print(*range(10), sep = ",") #prints 0,1,2,3,4,5,6,7,8,9
+
+#----------------------------packing the input parameter of a function and passing tupple as an argument to the function-------
+Total = 0
+def summation(*numbers):
+    for number in numbers:
+        global Total
+        Total+=number
+    print(Total) 
+    
+ 
+summation(0,10,20,30) # we must see the result as 60
+    
+
+
