b36896be64abd7891f11c584164fbb3f6d2754ed
PoornaPragnaMS <poorna220192@gmail.com>
2017-08-08 16:30:18 +0530

    Update PackingUnpackingInPython.py

--- PackingUnpackingInPython.py
+++ PackingUnpackingInPython.py
@@ -1,34 +1,33 @@
 ##In this program we are going to see iterables in python and how we can pack and unpack them
 
-#--------------------------Let us consider a primitve iterable data type as our first case: String ---------------------------
+#1: Let us consider a primitve iterable data type as our first case: String
 
-#Request an input string from End User
+    #Request an input string from End User and use strip function to remove trailing and leading spaces around the string
 
-#strip function is used to remove trailing and leading spaces around the string
+    inputStr = input("Enter your input to unpack it :").strip() #Assume the input entered is "Hello"
 
-inputStr = input("Enter your input to unpack it :").strip() #Assume the input entered is "Hello"
+    print(inputStr,sep=",") # printing without unpacking the string, Response here is "Hello"
+    print(*inputStr,sep=",") #printing after unpacking the string, Response here is "H,e,l,l,o" due to iterable property of string
 
-print(inputStr,sep=",") # printing without unpacking the string, Response here is "Hello"
-print(*inputStr,sep=",") #printing after unpacking the string, Response here is "H,e,l,l,o" due to iterable property of string
+#2: Let us consider the second iterable type List for unpacking operation
 
-#---------------------------Let us consider the second iterable type List for unpacking operation------------------------------
-inputList = ["Mango","Orange","Apple"]
-print(*inputList)
+    inputList = ["Mango","Orange","Apple"]
+    print(*inputList)
 
-#----------------------------Unpacking the range function----------------------------------------------------------------------
+#3: Unpacking the range function
 
-print(*range(10), sep = ",") #prints 0,1,2,3,4,5,6,7,8,9
+    print(*range(10), sep = ",") #prints 0,1,2,3,4,5,6,7,8,9
 
-#----------------------------packing the input parameter of a function and passing tupple as an argument to the function-------
-Total = 0
-def summation(*numbers):
-    for number in numbers:
-        global Total
-        Total+=number
-    print(Total) 
-    
- 
-summation(0,10,20,30) # we must see the result as 60
+#4: Packing the input parameter of a function and passing tupple as an argument to the function
+
+    Total = 0
+    def summation(*numbers):
+        for number in numbers:
+            global Total
+            Total+=number
+        print(Total) 
+
+    summation(0,10,20,30) # we must see the result as 60
     
 
 
